65617cf28c1dfae7c1607851f6ee40ac0f967adb
Hristo Gatsinski <gatsinski@gmail.com>
2021-12-08 21:07:51 +0200

    Add `QuestionService`

---
+++ src/app/services/QuestionService.ts
@@ -0,0 +1,36 @@
+import { Service } from "typedi";
+import axios from "axios";
+import { JsonConvert } from "json2typescript";
+
+import { environment } from "src/environments/environment";
+import { Question } from "models/Question";
+import { AnswerStatus } from "models/AnswerStatus";
+
+@Service()
+export class QuestionService {
+    async readRandomQuestion(): Promise<Question> {
+        const response = await axios.get(
+            `${environment.apiUrl}/questions/random/`
+        );
+        const jsonConvert = new JsonConvert();
+
+        return jsonConvert.deserialize(response.data, Question) as Question;
+    }
+
+    async validateAnswer(
+        question: Question,
+        answerIndex: number
+    ): Promise<AnswerStatus> {
+        const response = await axios.post(
+            `${environment.apiUrl}/questions/${question.uuid}/validate-answer/`,
+            { uuid: question.answers[answerIndex].uuid }
+        );
+
+        const jsonConvert = new JsonConvert();
+
+        return jsonConvert.deserialize(
+            response.data,
+            AnswerStatus
+        ) as AnswerStatus;
+    }
+}
