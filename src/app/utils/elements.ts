caa6fa4647a38006df4cd07979ff409ad06f61a3
Hristo Gatsinski <gatsinski@gmail.com>
2022-03-15 11:50:08 +0200

    Add createButton util

---
+++ src/app/utils/elements.ts
@@ -0,0 +1,28 @@
+import * as PIXI from "pixi.js";
+
+export function createButton(content: string): PIXI.Graphics {
+    const graphics = new PIXI.Graphics();
+
+    graphics.beginFill(0x32a852);
+    graphics.lineStyle(2, 0xcccccc);
+    graphics.drawRoundedRect(0, 0, 300, 50, 10);
+    graphics.pivot.set(graphics.width / 2, graphics.height / 2);
+
+    graphics.interactive = true;
+    graphics.buttonMode = true;
+
+    const text = new PIXI.Text(content, {
+        fontFamily: "Arial",
+        fontWeight: "bold",
+        fontSize: 24,
+        fill: 0xffffff,
+        align: "left",
+    });
+
+    text.pivot.set(text.width / 2, text.height / 2);
+    text.position = graphics.pivot.clone();
+
+    graphics.addChild(text);
+
+    return graphics;
+}
